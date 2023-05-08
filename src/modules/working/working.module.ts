901a471093e0c98fb99e286f921e8310ea11cf83
Jollyer <jollyer@outlook.kr>
2023-05-08 14:37:46 +0900

    공정 진행, 실투입 재고

--- src/modules/working/working.module.ts
+++ src/modules/working/working.module.ts
@@ -5,6 +5,8 @@ import { PlanController } from './api/plan.controller';
 import { TaskChangeService } from './service/task-change.service';
 import { TaskRetriveService } from './service/task-retrive.service';
 import { TaskController } from './api/task.controller';
+import { StockChangeService } from '../stock/service/stock-change.service';
+import { StockValidator } from '../stock/service/stock.validator';
 
 @Module({
   providers: [
@@ -12,6 +14,8 @@ import { TaskController } from './api/task.controller';
     PlanRetriveService,
     TaskChangeService,
     TaskRetriveService,
+    StockChangeService,
+    StockValidator,
   ],
   controllers: [PlanController, TaskController],
 })
