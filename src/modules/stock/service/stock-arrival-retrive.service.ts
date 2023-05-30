e8b0062791c47791e75d2eb80b5c0295afbe77b8
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-05-30 17:15:55 +0900

    도착예정재고 목록

--- src/modules/stock/service/stock-arrival-retrive.service.ts
+++ src/modules/stock/service/stock-arrival-retrive.service.ts
@@ -17,38 +17,41 @@ export class StockArrivalRetriveService {
     const items = await this.prisma.stockGroup.findMany({
       include: {
         stockGroupEvent: true,
-        orderStock: {
+        plan: {
           include: {
-            order: {
+            orderStock: {
               include: {
-                srcCompany: true,
-                dstCompany: true,
+                warehouse: {
+                  include: {
+                    company: true,
+                  }
+                },
+                dstLocation: {
+                  include: {
+                    company: true,
+                  }
+                },
+                order: {
+                  include: {
+                    srcCompany: true,
+                    dstCompany: true,
+                  }
+                },
+                product: {
+                  include: {
+                    paperDomain: true,
+                    manufacturer: true,
+                    paperGroup: true,
+                    paperType: true,
+                  }
+                },
+                packaging: true,
+                paperColorGroup: true,
+                paperColor: true,
+                paperPattern: true,
+                paperCert: true,
               }
-            },
-            product: {
-              include: {
-                paperDomain: true,
-                manufacturer: true,
-                paperGroup: true,
-                paperType: true,
-              }
-            },
-            packaging: true,
-            paperColorGroup: true,
-            paperColor: true,
-            paperPattern: true,
-            paperCert: true,
-            dstLocation: {
-              include: {
-                company: true,
-              }
-            },
-            warehouse: {
-              include: {
-                company: true,
-              }
-            },
-            plan: true,
+            }
           }
         },
         product: {
@@ -67,11 +70,10 @@ export class StockArrivalRetriveService {
       },
       where: {
         companyId,
-        isArrived: false,
         stockGroupEvent: {
           some: {
             status: {
-              not: 'CANCELLED',
+              not: 'PENDING',
             }
           }
         }
@@ -87,9 +89,9 @@ export class StockArrivalRetriveService {
           return prev + cur.change;
         }, 0);
 
-      const orderCompany = (item.orderStock?.order.srcCompany.id === companyId ?
-        item.orderStock?.order.dstCompany :
-        item.orderStock?.order.srcCompany) || null;
+      const orderCompany = (item.plan?.orderStock?.order?.srcCompany.id === companyId ?
+        item.plan?.orderStock?.order?.dstCompany :
+        item.plan?.orderStock?.order?.srcCompany) || null;
 
       return {
         id: item.id,
@@ -105,27 +107,27 @@ export class StockArrivalRetriveService {
         paperCert: item.paperCert,
         warehouse: null, // 도착예정재고 => 창고 null
         orderCompanyInfo: orderCompany,
-        orderInfo: item.orderStock?.order ? {
-          wantedDate: item.orderStock.order.wantedDate.toISOString(),
+        orderInfo: item.plan?.orderStock?.order ? {
+          wantedDate: item.plan?.orderStock?.order?.wantedDate.toISOString(),
         } : null,
-        orderStock: item.orderStock ? {
-          id: item.orderStock.id,
-          orderId: item.orderStock.orderId,
-          dstLocation: item.orderStock.dstLocation,
-          warehouse: item.orderStock.warehouse,
-          product: item.orderStock.product,
-          packaging: item.orderStock.packaging,
-          grammage: item.orderStock.grammage,
-          sizeX: item.orderStock.sizeX,
-          sizeY: item.orderStock.sizeY,
-          paperColorGroup: item.orderStock.paperColorGroup,
-          paperColor: item.orderStock.paperColor,
-          paperPattern: item.orderStock.paperPattern,
-          paperCert: item.orderStock.paperCert,
-          quantity: item.orderStock.quantity,
-          plan: item.orderStock.plan ? {
-            id: item.orderStock.planId,
-            planNo: item.orderStock.plan.planNo,
+        orderStock: item.plan?.orderStock ? {
+          id: item.plan.orderStock.id,
+          orderId: item.plan.orderStock.orderId,
+          dstLocation: item.plan.orderStock.dstLocation,
+          warehouse: item.plan.orderStock.warehouse,
+          product: item.plan.orderStock.product,
+          packaging: item.plan.orderStock.packaging,
+          grammage: item.plan.orderStock.grammage,
+          sizeX: item.plan.orderStock.sizeX,
+          sizeY: item.plan.orderStock.sizeY,
+          paperColorGroup: item.plan.orderStock.paperColorGroup,
+          paperColor: item.plan.orderStock.paperColor,
+          paperPattern: item.plan.orderStock.paperPattern,
+          paperCert: item.plan.orderStock.paperCert,
+          quantity: item.plan.orderStock.quantity,
+          plan: item.plan ? {
+            id: item.plan.id,
+            planNo: item.plan.planNo,
           } : null,
         } : null,
         totalQuantity,
