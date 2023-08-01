c04addbef157cf5198672675fac6bc484a8017aa
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-01 14:44:54 +0900

    계정정보 수정

--- src/modules/setting/api/account.controller.ts
+++ src/modules/setting/api/account.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Body,
+  Controller,
+  Get,
+  Put,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AccountRetriveService } from '../service/account.retrive.service';
 import { AccountChangeService } from '../service/account.change.service';
 import { AuthGuard } from 'src/modules/auth/auth.guard';
 import { AuthType } from 'src/modules/auth/auth.type';
 import { AccountResponse } from 'src/@shared/api/setting/account.response';
+import { AccountUpdateDto } from './dto/account.request';
 
 @Controller('/setting/account')
 export class AccountController {
@@ -15,6 +24,20 @@ export class AccountController {
   @Get()
   @UseGuards(AuthGuard)
   async get(@Request() req: AuthType): Promise<AccountResponse> {
+    return await this.retrive.get(req.user.id);
+  }
+
+  @Put()
+  @UseGuards(AuthGuard)
+  async update(
+    @Request() req: AuthType,
+    @Body() body: AccountUpdateDto,
+  ): Promise<AccountResponse> {
+    await this.change.updateAccount({
+      userId: req.user.id,
+      ...body,
+    });
+
     return await this.retrive.get(req.user.id);
   }
 }
