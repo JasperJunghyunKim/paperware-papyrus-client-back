c0404f644178c6da7be81e4493ebe71de691abc9
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-02 11:25:38 +0900

    비밀번호 조건 수정

--- src/validator/is-password.validator.ts
+++ src/validator/is-password.validator.ts
@@ -8,13 +8,14 @@ export function IsPassword(validationOptions?: ValidationOptions) {
       propertyName: propertyName,
       constraints: [],
       options: {
-        message: `${propertyName}는 영문,숫자,특수문자를 조합하여 10~30자리로 입력해주세요.`,
+        message: `${propertyName}는 영문,숫자를 조합하여 10~30자리로 입력해주세요.`,
         ...validationOptions,
       },
       validator: {
         validate(value: any) {
           const reg: RegExp =
-            /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10,30}$/;
+            // /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10,30}$/;
+            /^(?=.*[a-zA-Z])(?=.*[0-9]).{10,30}$/;
           return reg.test(value);
         },
       },
