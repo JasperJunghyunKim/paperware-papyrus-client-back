63554b95ba9055fbcdfbea079dab80388fe0cb3c
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-08 15:26:30 +0900

    계좌등록 버그 수정

--- src/modules/accounted/service/accounted-change.service.ts
+++ src/modules/accounted/service/accounted-change.service.ts
@@ -36,7 +36,7 @@ export class AccountedChangeService {
           isDeleted: false,
         },
       });
-      if (bankAccount)
+      if (!bankAccount)
         throw new NotFoundException(`존재하지 않는 계좌 정보입니다.`);
 
       return await tx.accounted.create({
